8d6906d2cf7ebbb30864fad81432b16ab442750d
duhao <duhao@longshine.com>
2021-04-08 00:12:30 +0800

    add input

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,21 +7,44 @@ export default function Index() {
 
     const [ user ] = useState(new UserAccount('DueTy', 123))
     const [list, setList] = useState<UserAccount[]>([])
+    const [searchKey, setSearchKey] = useState('')
 
     return (
         <div>
-            <div 
+            <div style={{ marginTop: 10 }}>
+                <input type="text" placeholder="搜索" value={searchKey} />
+                <button 
+                    onChange={handleSearchKeyChange}
+                    onClick={handleSearchClick} 
+                    style={{ marginLeft: 10 }}>搜索</button>
+            </div>
+            <h1 
                 className="main-user"
                 onClick={handleFirstClick}>
                 {user.name}-{user.id}
-            </div>
+            </h1>
             {list.map((item, index) => (
-            <div key={index}>{item.name}-{item.id}</div>
+            <h1 key={index}>{item.name}-{item.id}</h1>
             ))}
         </div>
     )
+
+    function handleSearchKeyChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setSearchKey(e.target.value)
+    }
     
     function handleFirstClick() {
-        setList([ ...list, new UserAccount('DueTy', user.id + 1)])
+        const lastId = list[list.length - 1] ? list[list.length - 1].id : user.id
+        setList([ ...list, new UserAccount('DueTy', lastId + 1)])
+    }
+
+    function handleSearchClick() {
+        console.log(searchKey);
+        
+    }
+
+    function getUserById(id:number): UserAccount {
+        const foundUser = list.find(u => u.id === id) || new UserAccount('DueTy', id)
+        return foundUser
     }
 }
